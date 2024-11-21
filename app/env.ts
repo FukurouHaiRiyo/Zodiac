69142f190997014e597c3be3b6c7c2be2ddf44ed
Fuku <53621037+FukurouHaiRiyo@users.noreply.github.com>
2024-11-21 17:52:26 +0200

    updates

---
+++ app/env.ts
@@ -0,0 +1,18 @@
+export const apiKey = "AIzaSyBbkF7khqhgWJ5zAegFne7pWYbCMQ5NYEo";
+export const authDomain = "emails-9d2a6.firebaseapp.com";
+export const databaseURL= "https://emails-9d2a6-default-rtdb.europe-west1.firebasedatabase.app";
+export const projectId= "emails-9d2a6";
+export const storageBucket= "emails-9d2a6.firebasestorage.app";
+export const messagingSenderId= "101385771332";
+export const appId= "1:101385771332:web:3af6af6a540ad0f0415f11";
+export const measurementId= "G-EQYRVFL2D9"
+
+export const XRapidApiKey = '1591618643mshd45411114ae3579p181d33jsn4a85dd05e1ea';
+export const XRapidApiHost = 'horoscope19.p.rapidapi.com';
+
+export const translateApi = '1591618643mshd45411114ae3579p181d33jsn4a85dd05e1ea';
+export const translateHost = 'microsoft-translator-text-api3.p.rapidapi.com';
+
+export const app_pass = 'tqaqvxffuyormfiv'
+
+export const user = 'andreipanait00@gmail.com'
