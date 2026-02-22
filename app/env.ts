d3fd04abed538d1e22db40f38db393bfa7febd2c
Fuku <53621037+FukurouHaiRiyo@users.noreply.github.com>
2026-02-22 20:32:32 +0200

    Update env.ts

--- app/env.ts
+++ app/env.ts
@@ -1,12 +1,3 @@
-export const apiKey = "";
-export const authDomain = "emails-9d2a6.firebaseapp.com";
-export const databaseURL= "https://emails-9d2a6-default-rtdb.europe-west1.firebasedatabase.app";
-export const projectId= "emails-9d2a6";
-export const storageBucket= "emails-9d2a6.firebasestorage.app";
-export const messagingSenderId= "101385771332";
-export const appId= "1:101385771332:web:3af6af6a540ad0f0415f11";
-export const measurementId= "G-EQYRVFL2D9"
-
 export const XRapidApiKey = '1591618643mshd45411114ae3579p181d33jsn4a85dd05e1ea';
 export const XRapidApiHost = 'horoscope-astrology.p.rapidapi.com';
 
